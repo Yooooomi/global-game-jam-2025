41a6db848859ad8ddbafcee28adcf62340d3b55e
noerls <noerls@google.com>
2025-01-25 02:15:44 +0100

    Added bubble sound

--- Assets/Scripts/BubbleHittable.cs
+++ Assets/Scripts/BubbleHittable.cs
@@ -2,8 +2,36 @@ using UnityEngine;
 
 public class BubbleHittable : MonoBehaviour, Hittable
 {
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Rigidbody2D body;
+    [SerializeField]
+    private Collider2D selfCollider;
+    [SerializeField]
+    private RandomSound toPlayOnDeath;
+
+    [SerializeField]
+    private float dieAnimationTime;
+
+    private float killAtTime = 0f;
+
+    private void Kill() {
+        selfCollider.enabled = false;
+        body.constraints = RigidbodyConstraints2D.FreezeAll;
+        killAtTime = Time.time + dieAnimationTime;
+        animator.SetBool("Dead", true);
+        toPlayOnDeath.PlayRandom();
+    }
+
     public void Hit(float damage)
     {
-        Destroy(gameObject);
+        Kill();
+    }
+
+    public void Update() {
+        if (killAtTime != 0 && Time.time > killAtTime) {
+            Destroy(gameObject);
+        }
     }
 }
