be1498d387c46e06193c9496cf6dbf2b20dc8fc4
noerls <noerls@google.com>
2025-01-26 17:01:57 +0100

    build

--- Assets/Scripts/Blow/Blowable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Blowable
-{
-    public void Blow(Vector3 from, float force);
-}
--- Assets/Scripts/Hittable.cs
+++
@@ -1,5 +0,0 @@
-public interface Hittable
-{
-    // Returns experience gained if killed, -1 otherwise
-    public int Hit(float damages);
-}
